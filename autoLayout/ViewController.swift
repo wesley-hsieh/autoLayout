c65ae487b2ef77cbb03ff2852d247db247a8f52d
Wesley <whsieh1996@gmail.com>
2020-07-04 20:11:57 -0700

    Initial Commit

---
+++ autoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autoLayout
+//
+//  Created by Wesley on 7/4/20.
+//  Copyright © 2020 Wesley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
